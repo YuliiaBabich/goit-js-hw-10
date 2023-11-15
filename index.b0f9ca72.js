c36d4b94b609172b3bd2a304947873c8a2241c42
YuliiaBabich <122433977+YuliiaBabich@users.noreply.github.com>
2023-11-15 20:58:58 +0000

    Deploying to gh-pages from @ YuliiaBabich/goit-js-hw-10@383aedbaf93f95f94e72887e41c2ca5ea5441f4e 🚀

--- index.b0f9ca72.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".cat-info");
-//# sourceMappingURL=index.b0f9ca72.js.map
--- index.dfcec345.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".cat-info");
-//# sourceMappingURL=index.dfcec345.js.map
