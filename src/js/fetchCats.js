1a09205529a53488dba7a1cc50d35469f72c1191
YuliiaBabich <aim2002@ukr.net>
2023-11-16 13:19:53 +0200

    z

--- src/js/fetchCats.js
+++ src/js/fetchCats.js
@@ -1,6 +1,13 @@
 /*export const fetchCats = name => {
   const BASE_URL ='https://api.thecatapi.com/v1/images/search?api_key=live_OpPmoOtiWW1fCPc5NywKSPInv8nblQsFJGbMFK0jB6J6drtIWqfkxxYsy8dtL1dy';
   const properties = 'fields=cat,name,temperament';*/
+
+/*function fetch() {
+  const BASE_URL = 'https://api.thecatapi.com/v1';
+  const End_Point = '/images/search'; 
+  const api_key = '?api_key=live_OpPmoOtiWW1fCPc5NywKSPInv8nblQsFJGbMFK0jB6J6drtIWqfkxxYsy8dtL1dy';
+  const URL = '${BASE_URL}${End_Point}${api_key}' 
+  }*/
 const URL = `https://api.thecatapi.com/v1`;
 export function fetchBreeds() {
   return fetch(`${URL}/breeds`).then(response => {
