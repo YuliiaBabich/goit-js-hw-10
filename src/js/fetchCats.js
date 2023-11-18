151f267dd1400e9e72eb848154014d752e8170e2
YuliiaBabich <aim2002@ukr.net>
2023-11-18 18:14:11 +0200

    1

--- src/js/fetchCats.js
+++ src/js/fetchCats.js
@@ -1,13 +1,36 @@
-/*export const fetchCats = name => {
-  const BASE_URL ='https://api.thecatapi.com/v1/images/search?api_key=live_OpPmoOtiWW1fCPc5NywKSPInv8nblQsFJGbMFK0jB6J6drtIWqfkxxYsy8dtL1dy';
-  const properties = 'fields=cat,name,temperament';*/
-
-/*function fetch() {
-  const BASE_URL = 'https://api.thecatapi.com/v1';
+/*function getObjekt(symbol) {
+const BASE_URL = 'https://api.thecatapi.com/v1';
   const End_Point = '/images/search'; 
-  const api_key = '?api_key=live_OpPmoOtiWW1fCPc5NywKSPInv8nblQsFJGbMFK0jB6J6drtIWqfkxxYsy8dtL1dy';
-  const URL = '${BASE_URL}${End_Point}${api_key}' 
-  }*/
+  const PARAMS = `?symbol=${symbol}`;
+  const url = `${BASE_URL}${END_POINT}${PARAMS}`;
+  //const api_key = '?api_key=live_OpPmoOtiWW1fCPc5NywKSPInv8nblQsFJGbMFK0jB6J6drtIWqfkxxYsy8dtL1dy';
+  //const urlKey = `${BASE_URL}${END_POINT}${PARAMS}${api_key}`;
+  const options = {
+    headers: {
+      'x-api-key': 'live_OpPmoOtiWW1fCPc5NywKSPInv8nblQsFJGbMFK0jB6J6drtIWqfkxxYsy8dtL1dy',
+        },
+   // const URL = 'https://api.thecatapi.com/v1/images/search?api_key=live_OpPmoOtiWW1fCPc5NywKSPInv8nblQsFJGbMFK0jB6J6drtIWqfkxxYsy8dtL1dy';
+  }
+
+refs.formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const query = e.target.elements.query.value;
+  getPrice(query).then(result => {
+    renderTicker(result);
+  });
+});
+  return fetch(url, options).then(res => res.json());
+}
+
+function renderCats({ objekt, symbol }) {
+  const markup = `
+  <span>${symbol}</span>
+  <span>${(+price).toFixed(2)}</span>
+  `;
+  refs.infoEl.innerHTML = markup;
+}
+*/
+
 const URL = `https://api.thecatapi.com/v1`;
 export function fetchBreeds() {
   return fetch(`${URL}/breeds`).then(response => {
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix';
 
 export function markupCreator(imgUrl, name, description, temperament) {
   return `
-      <img style='margin-right: 20px' src=${imgUrl} width='400px'>
+      <img style='margin-right: 20px' src=${imgUrl} width='450px'>
       <div >
         <h2>${name}</h2>
         <p>${description}</p>
